25ce7949f7fc59c3614f4fe4d5ffe07ac2277ba2
CynthiaY209 <yueyixuan0414@163.com>
2020-10-11 00:24:36 +0800

    A2

---
+++ Week2/Project/EscapingWheel/Assets/Scripts/Events.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Home))
+        {
+            Application.Quit();
+        }
+    }
+}
