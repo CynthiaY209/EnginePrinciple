af29cafa2a0aae29b10b56ad54cf9f612942626f
CynthiaY209 <yueyixuan0414@163.com>
2020-12-04 02:21:33 +0800

    Update

---
+++ ZeldaSG/Assets/Models/Mobile - Jojos Stray Cat 1/SCControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SCControl : MonoBehaviour
+{
+    public GameObject go;
+    private Material material;
+    private float dissolve;
+    void Start()
+    {
+        material = go.GetComponent<MeshRenderer>().material;
+        dissolve = 0;
+        
+        DOTween.To(() => dissolve, X => dissolve = X, 1.0f, 10);
+    }
+    private void Update()
+    {
+        material.SetFloat("_Dissolve", dissolve);
+    }
+}
---
+++ ZeldaSG/Assets/Scripts/FXController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXController : MonoBehaviour
+{
+    public ParticleSystem FXLogoOutline;
+    public ParticleSystem FXWall1;
+    public ParticleSystem FXLogo;
+    public ParticleSystem FXWall2;
+    public ParticleSystem FXWall3;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Anima());
+        }
+    }
+    IEnumerator Anima()
+    {
+        FXLogo.Play();
+        FXWall2.Play();
+        FXWall3.Play();
+        //yield return new WaitForSeconds(0.2f);
+        FXLogoOutline.Stop();
+        yield return new WaitForSeconds(2.0f);
+        FXWall1.Stop();
+    }
+}
